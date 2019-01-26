ad230728b65805119cbf53b947b21a1ffc71429d
Ugo <ugo.briasco@sumup.com>
2019-01-26 22:21:29 +0100

    Invoices

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 * Primary file for API
 */
 
+const exec = require("child_process").exec;
+
 // Dependancies
 const server = require("./lib/server");
 
 // Declare the main application
 const app = {
   init: function() {
+    // Initialize directories
+    exec("sh ./scripts/init.sh", (error, stdout, stderr) => {
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+      }
+    });
+
     // Start the server
     server.start();
   }
---
+++ scripts/init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p .data/carts
+mkdir -p .data/orders
+mkdir -p .data/users
+mkdir -p .data/tokens
+mkdir -p .logs
